77cfd50423684c20d7e3aaa849d6a211f7a2d1d6
SeacenLiu <1016667972@qq.com>
2019-03-26 23:49:31 +0800

    init

---
+++ SCNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCNotification
+//
+//  Created by SeacenLiu on 2019/3/26.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCNotification
+//
+//  Created by SeacenLiu on 2019/3/26.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
